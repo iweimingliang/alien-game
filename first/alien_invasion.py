c31eb6bdcae509665db3f50ca38ccc8fc4a2b755
iweimingliang <2549988709@qq.com>
2019-08-22 17:48:12 +0800

    添加play按钮

--- first/alien_invasion.py
+++ first/alien_invasion.py
@@ -7,6 +7,9 @@ import game_functions as gf
 from settings import Settings
 from pygame.sprite import Group
 from ship import Ship
+from alien import Alien
+from game_stats import GameStats
+from button import Button
 
 
 def run_game():
@@ -16,22 +19,33 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
+    play_button = Button(ai_settings,screen,"Play")
+
+    # 创建一个用于存储游戏统计信息的实例
+    stats = GameStats(ai_settings)
+
+    alien = Alien(ai_settings,screen)
 
     # 创建一膄飞船
     ship = Ship(ai_settings,screen)
 
     bullets = Group()
+    aliens = Group()
+
+    # 创建外星人群
+    gf.create_fleet(ai_settings,screen,ship,aliens)
 
     # 开始游戏的主循环
     while True:
-        
         #监视键盘和鼠标事件
-        gf.check_events(ai_settings,screen,ship,bullets)
-        ship.update()
-        bullets.update()
+        gf.check_events(ai_settings,screen,stats,play_button,ship,aliens,bullets)
 
-        gf.update_bullets(bullets)
+        if stats.game_active:
+            ship.update()
+            bullets.update()
+            gf.update_bullets(ai_settings,screen,ship,aliens,bullets)
+            gf.update_aliens(ai_settings,stats,screen,ship,aliens,bullets)
 
-        gf.update_screen(ai_settings,screen,ship,bullets)
-
+        gf.update_screen(ai_settings,screen,stats,ship,aliens,bullets,play_button)
+       
 run_game()
